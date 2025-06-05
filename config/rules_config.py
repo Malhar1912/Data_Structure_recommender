4d06f335b672fc78cc9bd18130bc22031df38e07
malhar1912 <malharpangarkar08@gmail.com>
2025-06-05 13:16:31 +0530

    Add rules_config.py defining keyword semantics for FSM insights

---
+++ config/rules_config.py
@@ -0,0 +1,9 @@
+structure_semantics = {
+    "stack": ["backtrack", "undo", "nested", "reverse"],
+    "queue": ["order", "wait", "bfs", "process"],
+    "heap": ["priority", "max", "min", "greedy"],
+    "hash table": ["lookup", "index", "cache", "map"],
+    "graph": ["node", "edge", "path", "connection"],
+    "tree": ["hierarchy", "parent", "child", "recursive"],
+    "trie": ["prefix", "autocomplete", "search"]
+}
